a5f16301342f68b89b908e843a130fa31d469d09
Tolqinov Otabek <otabektolqinov30@gmail.com>
2024-10-16 16:16:53 +0500

    Created project Airport Management System

---
+++ src/test/java/com/airport/Airport/Management/System/AirportManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airport.Airport.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
